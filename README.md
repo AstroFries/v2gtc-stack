596942b6d8b77d25a73a8a55b6fc4b0a14dc2372
SX-9 <satriakrach@gmail.com>
2024-01-10 19:42:53 +0700

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# V2GTC Stack
+
+Template for a website using Vue, Vite, Tailwind, Github Pages, and Catppuccin.
+
+## Usage
+
+1. Fork and Clone this repo
+2. Run `npm install`
+3. Run `npm run dev` to start the dev server
+4. Modify the files to your liking
+5. Edit the `vite.config.js` file and edit the `base` property to your repo name (if you want to use a custom domain, skip this and go to [Custom Domain](#custom-domain) then continue to step 6)
+6. Push to your repo and enable Github Pages in the settings and select source as github actions
+7. Enjoy your new website!
+
+## Custom Domain
+
+To use a custom domain, create a file called `CNAME` in the `public` folder and put your domain in it. Then, go to your domain registrar and create a CNAME record pointing to your Github Pages URL.
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      techs: [
+        { name: 'Vue', link: 'https://vuejs.org/' },
+        { name: 'Vite', link: 'https://vitejs.dev/' },
+        { name: 'Tailwind', link: 'https://tailwindcss.com/' },
+        { name: 'Catppuccin', link: 'https://tailwindcss.catppuccin.com/' },
+        { name: 'Github Pages', link: 'https://pages.github.com/'},
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 class="text-3xl text-gradient from-green via-sapphire to-mauve">
+      <span v-for="(tech, i) in techs">
+        <span v-if="i"> + </span>
+        <a :href="tech.link" target="_blank" class="font-extrabold">{{ tech.name }}</a>
+      </span>
+    </h1>
+  </div>
+</template>
